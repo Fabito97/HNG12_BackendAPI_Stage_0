2ab30f559ee9620a6b32cd075147a7cf2c306636
fabian <fabbenco97@gmail.com>
2025-01-29 01:30:16 +0100

    Setup project

---
+++ HNG12BackendAPIStage0/Controllers/HNGInternController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNG12BackendAPIStage0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HNGInternController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            var info = new
+            {
+                Email = "fabbenco97@gmail.com",
+                CurrentDate = DateTime.UtcNow.ToString("yyyy-MM-ddTHH-mm-ssZ"),
+                GithubURL = "http://github.com/fabito97",
+            };
+
+            return Ok(info);
+        }
+    }
+}
