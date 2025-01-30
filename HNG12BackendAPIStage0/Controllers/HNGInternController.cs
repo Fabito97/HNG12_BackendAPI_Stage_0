90424cbcc9ed3952c446c14006b8e8940a43c3af
fabian <fabbenco97@gmail.com>
2025-01-30 19:08:21 +0100

    Update the route for the controller

--- HNG12BackendAPIStage0/Controllers/HNGInternController.cs
+++ HNG12BackendAPIStage0/Controllers/HNGInternController.cs
@@ -4,7 +4,7 @@ using static System.Net.WebRequestMethods;
 
 namespace HNG12BackendAPIStage0.Controllers
 {
-    [Route("api/hngintern")]
+    [Route("hngintern")]
     [ApiController]
     public class HNGInternController : ControllerBase
     {
